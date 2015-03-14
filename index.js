ae60c38932ebcc51b90dfc6f3589b67e97125ace
Jessica Lord <jessica.lord@github.com>
2015-03-14 13:07:49 -0700

    testing

---
+++ index.js
@@ -0,0 +1,25 @@
+// var Tabletop = require('tabletop')
+// var fs = require('fs')
+
+console.log("HELLO APP")
+
+// module.exports = function fetch(keys, time) {
+//   var time = time || 12 * 60 * 60 * 1000
+//
+//   // setTimeout(fetchSheets, time)
+//
+//   fetchSheets(keys)
+//
+//   // use tabletop to retrieve lovely json from each google spreadsheet
+//   function fetchSheets(keys) {
+//     keys.forEach(function(key) {
+//       var opts = {key: key, callback: writeSheets, simpleSheet: true}
+//       Tabletop.init(opts)
+//     })
+//   }
+//
+//   // write each sheet to file
+//   function writeSheets(data, tabletop) {
+//     console.log("TT", tabletop.key)
+//   }
+// }
