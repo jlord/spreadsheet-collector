2c518b77dc44beba05fa098f90c7d6d8a78c273f
Jessica Lord <jessica.lord@github.com>
2015-03-15 18:00:44 -0700

    a db starting place...

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var http = require('http')
-var config = require('./config.json')
-
+var Tabletop = require('tabletop')
 var pg = require('pg')
 
-// var writeData =  require('./writedata.js')
-
-// writeData(config.keys, config.timer)
+var config = require('./config.json')
 
 var server = http.createServer(route)
+pg.defaults.ssl = true
 
 server.listen(process.env.PORT || 5000, function listening() {
   console.log("Server serving...")
@@ -15,22 +13,35 @@ server.listen(process.env.PORT || 5000, function listening() {
 
 function route(req, res) {
 
-  // if (req.url === ('/')) {
-  //   res.statusCode = 200
-  //   res.end("HELLO")
-  //   return res
-  // }
-
-  if (req.url === ('/db')) {
-    pg.connect(process.env.HEROKU_POSTGRESQL_ROSE_URL, function(err, client, done) {
-      if (err) return console.error('error fetching client from pool', err)
-      client.query('SELECT * FROM test_table', function(err, result) {
-        done()
-        if (err)
-         { console.error(err); res.send("Error " + err) }
-        else
-         { console.log(result.rows); res.send(result.rows) }
-      })
-    })
+  if (req.url === ('/')) {
+    res.statusCode = 200
+    res.end("Hai.")
+    return res
   }
+
+  if (req.url === ('/fetch')) {
+
+    config.keys.forEach(function(key) {
+      var sskey = Object.keys(key)
+      var opts = {key: sskey[0], callback: writeDatabase, simpleSheet: true}
+      Tabletop.init(opts)
+    })
+
+    // TABLE SPREADSHEETS(SSKEY, SSNAME, SSJSON);
+    function writeDatabase(data, tabletop) {
+      console.log(tabletop.foundSheetNames)
+      pg.connect(process.env.HEROKU_POSTGRESQL_ROSE_URL, function(err, client, done) {
+        if (err) return console.error('error fetching client from pool', err)
+        var querytext = 'INSERT INTO spreadsheets VALUES ($1,$2,$3)'
+        client.query(querytext, [tabletop.key, tabletop.foundSheetNames, JSON.stringify(data)], function(err, result) {
+          done()
+          if (err)
+           { console.error(err); res.end("Error " + err) }
+          else
+           { res.end("Great. Thanks.") }
+        })
+      })
+    }
+  }
+
 }
